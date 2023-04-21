1d598a2d18fb04b2f5db439742db17a39b938243
Jun Yamaguchi <jun_7104@outlook.jp>
2023-04-21 07:13:46 +0000

    Finish Static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,24 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
-    it "returns http success" do
-      get "/static_pages/home"
-      expect(response).to have_http_status(:success)
+  let (:base_title) {"Serve"}
+
+  describe "#root" do
+    it "rootにアクセスでhomeを表示すること" do
+      get root_url
+      expect(response).to have_http_status :ok
     end
   end
 
-  describe "GET /about" do
-    it "returns http success" do
-      get "/static_pages/about"
-      expect(response).to have_http_status(:success)
+  describe "#home" do
+    it "HTTPレスポンスを正常に返すこと home" do
+      get static_pages_home_path
+      expect(response).to have_http_status :ok
+    end
+
+    it "ホーム | Serve が含まれていること" do
+     get static_pages_home_path
+     expect(response.body).to include "ホーム | #{base_title}"
     end
   end
 
-  describe "GET /contact" do
-    it "returns http success" do
-      get "/static_pages/contact"
-      expect(response).to have_http_status(:success)
+  describe "#about" do
+    it "HTTPレスポンスを正常に返すこと about" do
+      get static_pages_about_path
+      expect(response).to have_http_status :ok
+    end
+
+    it "アプリについて | Serve が含まれていること" do
+     get static_pages_about_path
+     expect(response.body).to include "アプリについて | #{base_title}"
+    end
+  end
+
+  describe "#contact" do
+    it "HTTPレスポンスを正常に返すこと contact" do
+      get static_pages_contact_path
+      expect(response).to have_http_status :ok
+    end
+
+    it "問い合わせ | Serve が含まれていること" do
+     get static_pages_contact_path
+     expect(response.body).to include "問い合わせ | #{base_title}"
     end
   end
 
