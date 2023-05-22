c49afcb52729d610d6721c10cdce3f72c4e1d868
Jun Yamaguchi <jun_7104@outlook.jp>
2023-05-22 12:25:02 +0000

    フォロー機能の実装とHotwireの導入

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  # get    "/newpost",   to: "beansposts#new"
   resources :beansposts,    only: [:index, :new, :create, :destroy]
   resources :relationships,  only: [:create, :destroy]
 end
