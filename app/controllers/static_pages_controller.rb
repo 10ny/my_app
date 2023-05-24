c296db0873668349703b2b47b2120d2dbc6d4076
Jun Yamaguchi <jun_7104@outlook.jp>
2023-05-24 07:39:43 +0000

    フォロー中の投稿一覧の実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @beansposts = Beanspost.all.page(params[:page]).per(12) # ページネーションはポスト12個ごとの表示に設定
+      @beansposts = current_user.feed.page(params[:page]).per(12) # ページネーションはポスト12個ごとの表示に設定
       @beanspost = current_user.beansposts.build
-      @feed_items = current_user.feed.page(params[:page]).per(12) # ページネーションはポスト12個ごとの表示に設定
     end
   end
 
