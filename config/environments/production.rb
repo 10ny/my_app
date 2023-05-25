3c4f6148cc5104dbea48520a06b834256f62bf03
Jun Yamaguchi <jun_7104@outlook.jp>
2023-05-25 07:47:57 +0000

    ホストの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = https://serve-l6h3.onrender.com
+  host = 'https://serve-l6h3.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
