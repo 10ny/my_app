4666adcaadc165580ea2dce38158f96bf596518a
Jun Yamaguchi <jun_7104@outlook.jp>
2023-05-26 13:13:47 +0000

    投稿機能追加

--- app/models/beanspost.rb
+++ app/models/beanspost.rb
@@ -7,6 +7,11 @@ class Beanspost < ApplicationRecord
   # 投稿の表示順を降順に指定
   default_scope -> { order(created_at: :desc) }
   
+  validates :product_name, presence: true
+
+  validates :country, presence: true
+  validates :price,   numericality: true, inclusion: { only_integer: true, in: 100..10000 }
+  validates :grams,   numericality: true, inclusion: { only_integer: true, in: 10..10000 }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :image,   content_type: { in: %w[image/jpeg image/gif image/png], message: "その画像フォーマットは対応していません。"},
