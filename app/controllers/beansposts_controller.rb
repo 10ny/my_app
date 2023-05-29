65a32e119e68e18a83381049447dc80f24980ec4
Jun Yamaguchi <jun_7104@outlook.jp>
2023-05-29 05:38:55 +0000

    デフォルト画像の設定

--- app/controllers/beansposts_controller.rb
+++ app/controllers/beansposts_controller.rb
@@ -12,12 +12,15 @@ class BeanspostsController < ApplicationController
 
   def create
     @beanspost = current_user.beansposts.build(beanspost_params)
+      # if @beanspost.image.nil?
+      #   @beanspost.image = "default_post.png"#デフォルトで画像を設定
+      # end
     @beanspost.image.attach(params[:beanspost][:image])
     if @beanspost.save
       flash[:success] = "投稿しました！！"
       redirect_to root_url
     else
-      @feed_items = current_user.feed.page(params[:page]).per(12) # ページネーションはポスト12個ごとの表示に設定
+      # @feed_items = current_user.feed.page(params[:page]).per(12) # ページネーションはポスト12個ごとの表示に設定
       render "new", status: :unprocessable_entity
     end
   end
