cba0674c60d46aa5de096246e5aaee9e3fb87bb7
Jun Yamaguchi <jun_7104@outlook.jp>
2023-06-04 13:55:03 +0000

    ブックマーク機能更新

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,18 +2,20 @@ class BookmarksController < ApplicationController
 before_action :logged_in_user
 
   def create
-    @beanspost = Beanspost.new(params[:beanspost_id])
+    @beanspost = Beanspost.find(params[:beanspost_id])
     current_user.bookmark(@beanspost)
     respond_to do |format|
       format.html { redirect_to @beanspost }
       format.turbo_stream
+    end
   end
 
   def destroy
-    @beanspost = current_user.bookmarks.find(params[:beanspost_id]).beanspost
-    @beasnpost.destroy
+    @beanspost = current_user.bookmarks_beansposts.find(params[:beanspost_id]).beanspost
+    current_user.unbookmark(@beanspost)
     respond_to do |format|
       format.html { redirect_to @beanspost }
       format.turbo_stream
+    end
   end
 end
