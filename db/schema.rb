9a1150df69b2ca8309c8e3311b80cc5bb2075f44
Jun Yamaguchi <jun_7104@outlook.jp>
2023-06-13 05:26:25 +0000

    投稿ページ一部変更

--- db/schema.rb
+++ db/schema.rb
@@ -65,8 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_093829) do
     t.integer "beanspost_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index "\"user\", \"beanspost\"", name: "index_bookmarks_on_user_and_beanspost", unique: true
     t.index ["beanspost_id"], name: "index_bookmarks_on_beanspost_id"
+    t.index ["user_id", "beanspost_id"], name: "index_bookmarks_on_user_id_and_beanspost_id", unique: true
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
