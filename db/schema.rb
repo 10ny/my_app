0d97a39cdca494ccdcc6d7e308e76701c3cbb558
Jun Yamaguchi <jun_7104@outlook.jp>
2023-06-28 16:25:21 +0900

    db変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_02_093829) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,7 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_093829) do
 
   create_table "beansposts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "product_name"
@@ -61,8 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_093829) do
   end
 
   create_table "bookmarks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "beanspost_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "beanspost_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["beanspost_id"], name: "index_bookmarks_on_beanspost_id"
